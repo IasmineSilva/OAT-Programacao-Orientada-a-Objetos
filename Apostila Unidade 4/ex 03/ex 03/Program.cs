6fe6b6cf9c7277f8558642695e8e56b68c04b642
Iasmine <72636308+IasmineSilva@users.noreply.github.com>
2022-12-12 12:49:02 -0300

    entregando oat

---
+++ Apostila Unidade 4/ex 03/ex 03/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pop, filhos, total_filhos = 0, i, y = 1;
+            float sal, total_sal = 0, media_sal, media_filhos, maior_sal = 0, perc_sal;
+
+            Console.WriteLine("Qual a populacao da cidade..: ");
+            pop = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= pop; i++)
+            {
+                Console.WriteLine ("Qual o seu salario.........: R$ ");
+                sal = float.Parse(Console.ReadLine());
+                Console.WriteLine ("Qual a quantia de filhos...: ");
+                filhos = int.Parse(Console.ReadLine());
+
+                
+                total_sal = sal + total_sal;
+               
+                total_filhos = total_filhos + filhos;
+               
+                if (maior_sal < sal)
+                {
+                    maior_sal = sal;
+                }
+              
+                if (sal <= 1500)
+                {
+                    perc_sal = (y * 100) / pop;
+                    y++;
+                }
+            }
+            
+            media_sal = total_sal / pop;
+            
+            media_filhos = total_filhos / pop;
+
+            Console.WriteLine ("A media do salario da populacao eh R$ %.2f",+  media_sal);
+            Console.WriteLine ("A media do numero dos filhos eh %.1f", + media_filhos);
+            Console.WriteLine ("O maior salario eh R$ %.2f", + maior_sal);
+            Console.WriteLine ("A percentagem das pessoas com salarios ate R$ 240,00 eh %.1f%% " +  perc_sal);
+        }
+    }
+}
---
+++ Apostila Unidade 5/ex 03/ex 03/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_03
+{
+    internal class Program
+    {
+             private static int[] Vetor = new int[10];
+        public static void Main(string[] args)
+        {
+
+            bool continuar = true;
+            int opcao = 0;
+
+            do
+            {
+
+                Console.WriteLine("Digite 1 para carregar o vetor ");
+                Console.WriteLine("-------------------------------");
+
+                switch (opcao)
+                {
+                    case 1:
+                        CarregarVetor();
+                        break;
+                    case 2:
+                        Console.WriteLine(ListarVetor());
+                        break;
+                    case 3:
+                        Console.WriteLine(ListarParesVetor());
+                        break;
+                    case 4:
+                        Console.WriteLine(ListarImparesVetor());
+                        break;
+                    case 5:
+                        Console.WriteLine(QuantidadeParesNosImparesVetor());
+                        break;
+                    case 6:
+                        Console.WriteLine(QuantidadeImparesNosParesVetor());
+                        break;
+                    case 7:
+                        continuar = false;
+                        break;
+                }
+                Console.WriteLine("Digite uma opção: ");
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("2 - Para Listar o vetor");
+                Console.WriteLine("3 - Para Exibir os números pares do vetor");
+                Console.WriteLine("4 - Para Exibir os números ímpares do vetor");
+                Console.WriteLine("5 - Para Exibir a quantidade de números pares nas posições ímpares do vetor");
+                Console.WriteLine("6 - Para Exibir a quantidade de números ímpares nas posições pares do vetor");
+                Console.WriteLine("7 - Para Sair");
+                Console.WriteLine("---------------------------------");
+                opcao = int.Parse(Console.ReadLine());
+            } while (continuar);
+        }
+
+        public static void CarregarVetor()
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                Console.WriteLine("Digite o " + (x + 1) + "° número do vetor: ");
+                Vetor[x] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\nVetor carregado com sucesso!\n");
+        }
+
+        public static String ListarVetor()
+        {
+            String ListaVetor = "Valores do vetor:\n";
+            for (int x = 0; x < 10; x++)
+            {
+                ListaVetor += "\t" + Vetor[x] + "\n";
+            }
+            return ListaVetor;
+        }
+
+        public static String ListarParesVetor()
+        {
+            String ListaVetor = "Valores do vetor:\n";
+            for (int x = 0; x < 10; x++)
+            {
+                if (Vetor[x] % 2 == 0)
+                {
+                    ListaVetor += "\t" + Vetor[x] + "\n";
+                }
+            }
+            return ListaVetor;
+        }
+
+        public static String ListarImparesVetor()
+        {
+            String ListaVetor = "Valores do vetor:\n";
+            for (int x = 0; x < 10; x++)
+            {
+                if (Vetor[x] % 2 == 1)
+                {
+                    ListaVetor += "\t" + Vetor[x] + "\n";
+                }
+            }
+            return ListaVetor;
+        }
+
+        public static int QuantidadeParesNosImparesVetor()
+        {
+            int quantidade = 0;
+            for (int x = 1; x < 10; x = x + 2)
+            {
+                if (Vetor[x] % 2 == 0)
+                {
+                    quantidade++;
+                }
+            }
+            return quantidade;
+        }
+
+        public static int QuantidadeImparesNosParesVetor()
+        {
+            int quantidade = 0;
+            for (int x = 0; x < 10; x = x + 2)
+            {
+                if (Vetor[x] % 2 == 1)
+                {
+                    quantidade++;
+                }
+            }
+            return quantidade;
+        }
+    }
+}
+        
+    
+
